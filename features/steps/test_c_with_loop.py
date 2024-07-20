cd339102a5a9e4e2d1e549bcd346ee0d9d272409
Larisa N <larisale@gmail.com>
2024-07-19 18:49:54 -0700

    Howework Lesson 5

---
+++ features/steps/test_c_with_loop.py
@@ -0,0 +1,33 @@
+from selenium.webdriver.common.by import By
+from behave import given, then
+from time import sleep
+
+
+COLOR_OPTIONS = (By.CSS_SELECTOR, '[aria-label="Carousel"] ul li')
+SELECTED_COLOR = (By.CSS_SELECTOR, "[data-test='@web/VariationComponent'] div")
+
+
+@given('Open target product {product_id} page')
+def open_target(context, product_id):
+    context.driver.get(f'https://www.target.com/p/{product_id}')
+    sleep(8) #this step requires sleep command
+
+
+@then('Verify user can click through colors')
+def click_and_verify_colors(context):
+    expected_colors = ['Blue Tint', 'Denim Blue', 'Marine', 'Raven']
+    actual_colors = []
+
+    colors = context.driver.find_elements(*COLOR_OPTIONS)  # [webelement1, webelement2, webelement3]
+    # (By.CSS_SELECTOR, "[aria-label='Carousel']")
+    for color in colors[0:4]:
+        color.click()
+
+        selected_color = context.driver.find_element(*SELECTED_COLOR).text  # 'Color\nBlack'
+        print('Current color', selected_color)
+
+        selected_color = selected_color.split('\n')[1]  # remove 'Color\n' part, keep Black'
+        actual_colors.append(selected_color)
+        print(actual_colors)
+
+    assert expected_colors == actual_colors, f'Expected {expected_colors} did not match actual {actual_colors}'
